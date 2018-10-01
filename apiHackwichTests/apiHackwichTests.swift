26579c3546f27e09abf18a5f9dac5af2bcaea762
ALEJANDRO ARTEAGA <rl107407@rlas-macair-20.rlas-116.org>
2018-10-01 12:06:09 -0500

    Initial Commit

---
+++ apiHackwichTests/apiHackwichTests.swift
@@ -0,0 +1,36 @@
+//
+//  apiHackwichTests.swift
+//  apiHackwichTests
+//
+//  Created by ALEJANDRO ARTEAGA on 10/1/18.
+//  Copyright © 2018 ALEJANDRO ARTEAGA. All rights reserved.
+//
+
+import XCTest
+@testable import apiHackwich
+
+class apiHackwichTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
